201abe6273a731656a8da3338dd5ab6dfe19b3ad
Uwe L. Korn <uwelk@xhochy.com>
2012-03-12 23:39:58 +0100

    [Utils] Add fromStringSeqNode

--- Data/Yaml/SyckUtils.hs
+++ Data/Yaml/SyckUtils.hs
@@ -1,6 +1,6 @@
 -- |Utility functions for easier handling of YAML files.
 module Data.Yaml.SyckUtils (
-    fromStringNode,fromMapNode, fromSeqNode,
+    fromStringNode,fromMapNode, fromSeqNode, fromStringSeqNode,
     emapEntryVal, emapKeys
 ) where
 
@@ -21,6 +21,14 @@ fromSeqNode n = case (n_elem n) of
     ESeq s -> Just s
     _ -> Nothing
 
+-- |Convert a sequence of string nodes to a String array
+--
+-- If there are non-string nodes in this sequence, they will be ignored in the
+-- result. Only the given node is not a sequence node, this method will return
+-- Nothing.
+fromStringSeqNode :: YamlNode -> Maybe [String]
+fromStringSeqNode n = (fromSeqNode n) >>= (mapM fromStringNode)
+
 -- |Convert a YamlNode containing a map into a array with (key,value)-tuples
 fromMapNode :: YamlNode -> Maybe [(YamlNode, YamlNode)]
 fromMapNode n = case (n_elem n) of
@@ -32,7 +40,7 @@ emapEntryVal :: YamlNode -> String -> Maybe YamlNode
 emapEntryVal n mkey = Control.Monad.liftM snd ((fromMapNode n) >>= (List.find keyMatch))
     where keyMatch (k, v) = maybe False (\x -> x == mkey) (fromStringNode k)
 
--- |Get all Keys of a YamlNode
+-- |Get all Keys of a map node
 emapKeys :: YamlNode -> [String]
 emapKeys n = mapMaybe fromStringNode keys
     where keys = maybe [] (map fst) (fromMapNode n)
